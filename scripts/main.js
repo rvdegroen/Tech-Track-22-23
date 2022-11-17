bf4a8e21ca6fe798532cac63b2b013a6180a261e
rvdegroen <suiyichiro@hotmail.com>
2022-11-17 16:52:34 +0100

    edited console log texting text for server

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Our bundler automatically creates styling when imported in the main JS file!
-import '../styles/style.css'
+import "../styles/style.css";
 
 // We can use node_modules directely in the browser!
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
-console.log('Hello, world!');
+console.log("Testing the server succesful.");
