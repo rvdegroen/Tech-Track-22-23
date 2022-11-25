e58bbb9a4d92380f9d82934085ccd119d6887527
rvdegroen <suiyichiro@hotmail.com>
2022-11-25 08:38:30 +0100

    added functions for dropdown filter

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,81 @@
 // Importing
 // Our bundler automatically creates styling when imported in the main JS file!
 import "../styles/style.css";
-
 // We can use node_modules directely in the browser!
 import * as d3 from "d3";
 
 // Importing functions
 import { testFunction } from "./filter";
 
-// global
-/**My data of villagers*/
-const villagers = async () => {
+// Data of villagers
+/*const villagers = async () => {
   const response = await fetch("https://acnhapi.com/v1a/villagers/");
   const data = await response.json();
   console.log(data);
+};*/
+
+// Dropdown filter
+// $html elements
+const $species = document.getElementById("species");
+const $villagers = document.getElementById("villagers");
+
+const initializeSpecies = async () => {
+  // fetch villagers
+  const response = await fetch("https://acnhapi.com/v1/villagers");
+  // save response as json in variable
+  const villagersObject = await response.json();
+  const villagers = Object.values(villagersObject);
+
+  // use ... to convert array into a set of arguments of a function
+  // filter is to take something out of the object
+  // reduce maps and filters in a single pass
+  const allSpecies = villagers.reduce(
+    (accumulator, currentValue) => [...accumulator, currentValue.species],
+    []
+  );
+
+  // I don't want duplicate values from allSpecies and turn it into an array, so I use Set: https://www.samanthaming.com/tidbits/43-3-ways-to-remove-array-duplicates/
+  const uniqueSpecies = Array.from(new Set(allSpecies));
+  // Make for every value in the array: uniqueSpecies an option for dropdown filter
+  for (const species of uniqueSpecies) {
+    const option = document.createElement("option");
+    option.value = species;
+    option.textContent = species;
+    $species.appendChild(option);
+  }
 };
 
+initializeSpecies();
+
+const initializeVillagers = async () => {
+  const response = await fetch("https://acnhapi.com/v1/villagers");
+  const villagersObj = await response.json();
+  const villagers = Object.values(villagersObj);
+
+  for (const villager of villagers) {
+    const image = document.createElement("img");
+    image.src = villager.icon_uri;
+    $villagers.appendChild(image);
+  }
+};
+
+initializeVillagers();
+
+const filterVillagers = async (event) => {
+  $villagers.innerHTML = "";
+
+  const response = await fetch("https://acnhapi.com/v1/villagers");
+  const villagersObj = await response.json();
+  const villagers = Object.values(villagersObj);
+  const filteredVillagers = villagers.filter((villager) => villager.species === $species.value);
+
+  for (const villager of filteredVillagers) {
+    const image = document.createElement("img");
+    image.src = villager.icon_uri;
+    $villagers.appendChild(image);
+  }
+};
+
+$species.addEventListener("change", (event) => filterVillagers(event));
+
 // Calling functions
-villagers();
