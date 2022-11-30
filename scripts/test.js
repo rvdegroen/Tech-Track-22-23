1e1a18e83240790f9846d6064488ee45943d25af
rvdegroen <suiyichiro@hotmail.com>
2022-11-30 17:04:41 +0100

    changed filter.js to test.js

---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+//export functions
+export const testFunction = () => {
+  console.log("just testing my server");
+};
+
+// Importing functions in another file:
+// import { testFunction } from "./test";
