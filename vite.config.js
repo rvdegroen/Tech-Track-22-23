e8acad411c0865bf471255764ec4f3d04c36a479
rvdegroen <suiyichiro@hotmail.com>
2022-12-01 18:02:20 +0100

    configure vite from commonjs to es6

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-https://stackoverflow.com/questions/71544439/how-do-i-get-vite-to-build-entire-project-instead-of-just-the-index-html-page/71553448#71553448
-const { defineConfig } = require('vite')
+//stackoverflow.com/questions/71544439/how-do-i-get-vite-to-build-entire-project-instead-of-just-the-index-html-page/71553448#71553448
+import { defineConfig } from "vite";
 
-module.exports = defineConfig({
+// source: https://vitejs.dev/config/#config-intellisense
+export default defineConfig({
+  // ...
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        barChart: './graph.html',
-        bubbleChart: './bubbleChart.html',
-        
+        main: "./index.html",
+        barChart: "./graph.html",
+        bubbleChart: "./bubbleChart.html",
+
         // ...
         // List all files you want in your build
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
