1f650794ad488aa72a983a39a3caaadee4791ef9
rvdegroen <suiyichiro@hotmail.com>
2022-12-01 18:32:09 +0100

    commented out code

--- scripts/graph.js
+++ scripts/graph.js
@@ -206,8 +206,9 @@ const fetchVillagers = async () => {
 
 console.log(await fetchVillagers());
 
+/*
 // function to count total of different gender villagers
-const countGender = await fetchVillagers.reduce(accumulator, currentValue) {
+const countGender = await fetchVillagers.reduce(accumulator, currentValue) => {
   // accumulator + currentvalue, initialValue
   // make new accumulator for all villagers
   const newAccumulator = [...accumulator];
@@ -216,7 +217,7 @@ const countGender = await fetchVillagers.reduce(accumulator, currentValue) {
   // if 
   const (index === -1){newAccumulator.push({ gender, count: 1})}
   accumulator + currentValue, initialValue
-}
+});*/
 
 // function to count total of different personality villagers
 
