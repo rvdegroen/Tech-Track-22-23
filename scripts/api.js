7c66dc84e5f194552d341a1542c3ea37ae95c97d
rvdegroen <suiyichiro@hotmail.com>
2022-12-02 14:20:47 +0100

    added comment

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios is a replacement for fetch
-// more useful with API's that require authentication for ex.: cookies/ access tokens
+// more useful with API's that require authentication for ex.: cookies/ access tokens src: https://axios-http.com/docs/instance
 export const axiosInstance = axios.create({
   baseURL: "https://acnhapi.com/v1a",
 });
@@ -16,7 +16,7 @@ export const fetchVillagers = async () => {
   // return data;
 
   // new fetch function with axios so you don't have to write the whole URL
+  // src: https://stackoverflow.com/questions/48980380/returning-data-from-axios-api
   const response = await axiosInstance.get("/villagers");
   return response.data;
-  // to console.log the villagers, use call the function with await, otherwise it returns a promise
 };
